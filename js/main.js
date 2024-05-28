4dd4cfc8d28173e4361c63c3e620fd8f2b61df3f
NicolasKowal <nicolas.kowal95@gmail.com>
2024-05-28 15:41:12 -0300

    reemplazo de array por bbdd gustos.json

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
-const empanadas = [
-	{ tipo: "vegetarianas", empanada: "Española" },
-	{ tipo: "vegetarianas", empanada: "Capresse" },
-	{ tipo: "vegetarianas", empanada: "Verduras al horno" },
-	{ tipo: "vegetarianas", empanada: "Caponata" },
-	{ tipo: "vegetarianas", empanada: "Brócoli y queso azul" },
-	{ tipo: "vegetarianas", empanada: "Calabaza asada" },
-	{ tipo: "vegetarianas", empanada: "Champiñones a la provenzal" },
-	{ tipo: "vegetarianas", empanada: "Hongos y espinaca" },
-	{ tipo: "vegetarianas", empanada: "Queso y cebolla caramelizada" },
-	{ tipo: "vegetarianas", empanada: "Ratatuille" },
-	{ tipo: "veganas", empanada: "Berenjena y pimiento" },
-	{ tipo: "veganas", empanada: "Espárragos y tofu" },
-	{ tipo: "veganas", empanada: "Hongos y cebolla caramelizada" },
-	{ tipo: "veganas", empanada: "Papas al curry" },
-	{ tipo: "veganas", empanada: "Brócoli y tofu ahumado" },
-	{ tipo: "veganas", empanada: "Calabaza y maíz" },
-	{ tipo: "veganas", empanada: "Aceitunas y tomate seco" },
-	{ tipo: "veganas", empanada: "Alcachofa y espinaca" },
-	{ tipo: "veganas", empanada: "Lentejas y cebolla caramelizada" },
-	{ tipo: "veganas", empanada: "Zapallo italiano y berenjena" },
-];
 let ListaEmpanadasPedidas = [];
+let saboresDeEmpanadas =[];
 
 class Empanada {
 	constructor(tipo, cantidad) {
@@ -28,6 +7,22 @@ class Empanada {
 		this.cantidad = cantidad;
 	}
 }
+
+fetch("../db/gustos.json")
+	.then((response) => response.json())
+	.then((datos) => {
+		try {
+			datos.forEach((element) => {
+				saboresDeEmpanadas.push(element);
+			});
+		} catch (error) {
+			console.error("Error cargando los datos", error);
+		}
+	})
+	.catch((error) => {
+		console.error("Error al levantar el archivo", error);
+	});
+
 function Cookies() {
 	const pantalla = document.querySelector(".tomarPantalla");
 	pantalla.style.display = "flex";
@@ -198,13 +193,13 @@ function Empanadas() {
 		let botonVegana = document.querySelector("#botonVegana");
 
 		botonVegeta.addEventListener("click", () => {
-			let empanadaFiltro = empanadas.filter(
+			let empanadaFiltro = saboresDeEmpanadas.filter(
 				(empanada) => empanada.tipo == "vegetarianas"
 			);
 			GenerarLista(empanadaFiltro, Gustos);
 		});
 		botonVegana.addEventListener("click", () => {
-			let empanadaFiltro = empanadas.filter(
+			let empanadaFiltro = saboresDeEmpanadas.filter(
 				(empanada) => empanada.tipo == "veganas"
 			);
 			GenerarLista(empanadaFiltro, Gustos);
